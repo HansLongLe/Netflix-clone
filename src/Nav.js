cd53361dd38450809fac40d3d201bc4fef5b7dde
HansLongLe <HansLongLe@gmail.com>
2021-12-24 15:12:32 +0100

    removed black line from nav

--- src/Nav.js
+++ src/Nav.js
@@ -2,19 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 function Nav() {
-  const [show, handleShow] = useState(false);
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 200) {
-        handleShow(true);
-      } else handleShow(false);
-    });
-    return () => {
-      window.removeEventListener("scroll");
-    };
-  }, []);
   return (
-    <div className={`nav ${show && "nav_black"}`}>
+    <div className="nav">
       <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
